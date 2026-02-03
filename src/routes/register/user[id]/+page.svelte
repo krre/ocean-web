18c1e50c97ce39714e04c6745b2802ce06860330
Vladimir Zarypov <krre31@gmail.com>
2026-02-03 17:18:34 +0200

    Fix welcome page

--- src/routes/register/user[id]/+page.svelte
+++ src/routes/register/user[id]/+page.svelte
@@ -1,26 +1,17 @@
-<script module lang="ts">
-    import type { Session, Page } from "$lib/types";
-    export async function preload(page: Page, _session: Session) {
-        const { id } = page.params;
-        return { id };
-    }
-</script>
-
 <script lang="ts">
     import Frame from "$lib/components/Frame.svelte";
-    const title = "Регистрация окончена";
-    interface Props {
-        id: number;
-    }
+    import type { PageProps } from "./$types";
 
-    let { id }: Props = $props();
+    const title = "Регистрация окончена";
+
+    let { data }: PageProps = $props();
 </script>
 
 <Frame {title}>
-    <h2>Ваш ИД: {id}</h2>
+    <h2>Ваш ИД: {data.id}</h2>
 
     <p>Используйте его совместно с вашим паролем, чтобы входить на сайт.</p>
     Если вы потеряете ИД или пароль, то не сможете их восстановить, и вам придётся
     регистрироваться заново.
-    <p><a href="signin">Войти</a></p>
+    <p><a href="/signin">Войти</a></p>
 </Frame>
