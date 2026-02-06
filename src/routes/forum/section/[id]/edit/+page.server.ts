7da19006a544915bdf74ca7473674c1f7e600a46
Vladimir Zarypov <krre31@gmail.com>
2026-02-06 09:29:45 +0200

    Restore editing forum section

---
+++ src/routes/forum/section/[id]/edit/+page.server.ts
@@ -0,0 +1,12 @@
+import type { PageServerLoad } from './$types';
+import * as api from "$lib/api";
+
+export const load: PageServerLoad = async ({ params }) => {
+    const id = +params.id;
+    const getOneResponse = await api.Forum.Section.GetOne.exec({ id });
+
+    return {
+        id,
+        getOneResponse,
+    };
+}
--- src/routes/forum/section/[id]/edit/+page.svelte
+++ src/routes/forum/section/[id]/edit/+page.svelte
@@ -1,44 +1,28 @@
-<script module lang="ts">
-    import * as api from "$lib/api";
-    import type { Session, Page } from "$lib/types";
-
-    export async function preload(page: Page, _session: Session) {
-        const { id } = page.params;
-        const params: api.Forum.Section.GetOne.Request = {
-            id: Number(id),
-        };
-
-        const result = await api.Forum.Section.GetOne.exec(params);
-        const name = result.name;
-        const order = result.order_index;
-
-        return { id, name, order };
-    }
-</script>
-
 <script lang="ts">
     import { goto } from "$app/navigation";
     import * as route from "$lib/route";
+    import * as api from "$lib/api";
+    import { userSession } from "$lib/stores";
     import Frame from "$lib/components/Frame.svelte";
-    import SessionHub from "$lib/components/SessionHub.svelte";
     import SectionEditor from "$lib/components/forum/section/SectionEditor.svelte";
+    import type { PageProps } from "../$types";
 
     const title = "Редактировать раздел";
 
-    interface Props {
-        id: number;
-        name: string;
-        order: number;
-    }
+    const { data }: PageProps = $props();
 
-    let { id, name = $bindable(), order = $bindable() }: Props = $props();
+    let name = $state("");
+    let order = $state(0);
 
-    let isAdmin = $state(false);
+    $effect(() => {
+        name = data.getOneResponse.name;
+        order = data.getOneResponse.order_index;
+    });
 
     const action = async () => {
         const params: api.Forum.Section.Update.Request = {
-            id: +id,
-            name: name,
+            id: data.id,
+            name,
             order_index: order,
         };
 
@@ -47,10 +31,8 @@
     };
 </script>
 
-<SessionHub bind:isAdmin />
-
 <Frame {title}>
-    {#if !isAdmin}
+    {#if !$userSession.isAdmin}
         Доступ запрещён
     {:else}
         <SectionEditor bind:name bind:order {action} />
