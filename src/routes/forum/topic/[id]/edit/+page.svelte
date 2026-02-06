a272a3c2f29b3ddb813cc0406bc59c09e46a42b2
Vladimir Zarypov <krre31@gmail.com>
2026-02-06 09:47:05 +0200

    Restore editing forum topic

--- src/routes/forum/topic/[id]/edit/+page.svelte
+++ src/routes/forum/topic/[id]/edit/+page.svelte
@@ -1,56 +1,35 @@
-<script module lang="ts">
-    import * as api from "$lib/api";
-    import type { Session, Page } from "$lib/types";
-
-    export async function preload(page: Page, _session: Session) {
-        const { id } = page.params;
-        const params: api.Forum.Topic.GetOne.Request = {
-            id: +id,
-        };
-
-        const result = await api.Forum.Topic.GetOne.exec(params);
-        const name = result.name;
-        const sectionId = result.section_id;
-        const userId = result.user_id;
-
-        return { id, sectionId, name, userId };
-    }
-</script>
-
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import type { User } from "$lib/types";
     import * as route from "$lib/route";
+    import * as api from "$lib/api";
+    import type { PageProps } from "../$types";
+    import { userSession } from "$lib/stores";
     import Frame from "$lib/components/Frame.svelte";
-    import SessionHub from "$lib/components/SessionHub.svelte";
 
     const title = "Редактировать тему";
 
-    interface Props {
-        id: number;
-        sectionId: number;
-        name: string;
-        userId: number;
-    }
+    const { data }: PageProps = $props();
 
-    let { id, sectionId, name = $bindable(), userId }: Props = $props();
+    let name = $state("");
 
-    let isAdmin = $state(false);
-    let isAnonym = $state(true);
-    let user: User = $state();
+    $effect(() => {
+        name = data.getOneResponse.name;
+    });
 
     let editable = $derived(
-        isAdmin || (user && user.id === userId && !isAnonym),
+        $userSession.isAdmin ||
+            (!$userSession.isAnonym &&
+                $userSession.id === data.getOneResponse.user_id),
     );
 
     const action = async () => {
         const params: api.Forum.Topic.Update.Request = {
-            id: +id,
+            id: data.id,
             name: name,
         };
 
         await api.Forum.Topic.Update.exec(params);
-        goto(route.Forum.Section.Id(sectionId));
+        goto(route.Forum.Section.Id(data.getOneResponse.section_id));
     };
 </script>
 
@@ -61,15 +40,12 @@
     }
 </style>
 
-<SessionHub bind:isAdmin bind:isAnonym bind:user />
-
 <Frame {title}>
     {#if !editable}
         Доступ запрещён
     {:else}
         <div class="form">
             Название: <input type="text" bind:value={name} />
-
             <div>
                 <button onclick={action} disabled={!name}>Отправить</button>
             </div>
