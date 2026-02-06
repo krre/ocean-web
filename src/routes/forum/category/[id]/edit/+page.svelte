70d06fc061e3f9c433cfdc11d73cd93340437a06
Vladimir Zarypov <krre31@gmail.com>
2026-02-06 06:00:54 +0200

    Restore editing forum category

--- src/routes/forum/category/[id]/edit/+page.svelte
+++ src/routes/forum/category/[id]/edit/+page.svelte
@@ -1,42 +1,27 @@
-<script module lang="ts">
-    import * as api from "$lib/api";
-    import type { Session, Page } from "$lib/types";
-
-    export async function preload(page: Page, _session: Session) {
-        const { id } = page.params;
-        const params: api.Forum.Category.GetOne.Request = {
-            id: Number(id),
-        };
-
-        const result = await api.Forum.Category.GetOne.exec(params);
-        const name = result.name;
-        const order = result.order_index;
-
-        return { id, name, order };
-    }
-</script>
-
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import { userSession } from "$lib/stores";
     import * as route from "$lib/route";
+    import * as api from "$lib/api";
     import Frame from "$lib/components/Frame.svelte";
-    import SessionHub from "$lib/components/SessionHub.svelte";
     import CategoryEditor from "$lib/components/forum/category/CategoryEditor.svelte";
+    import type { PageProps } from "./$types";
 
     const title = "Редактировать категорию";
-    let isAdmin = $state(false);
 
-    interface Props {
-        id: number;
-        name: string;
-        order: number;
-    }
+    const { data }: PageProps = $props();
 
-    let { id, name = $bindable(), order = $bindable() }: Props = $props();
+    let name = $state("");
+    let order = $state(0);
+
+    $effect(() => {
+        name = data.getOneResponse.name;
+        order = data.getOneResponse.order_index;
+    });
 
     const action = async () => {
         const params: api.Forum.Category.Update.Request = {
-            id: +id,
+            id: data.id,
             name: name,
             order_index: order,
         };
@@ -46,10 +31,8 @@
     };
 </script>
 
-<SessionHub bind:isAdmin />
-
 <Frame {title}>
-    {#if !isAdmin}
+    {#if !$userSession.isAdmin}
         Доступ запрещён
     {:else}
         <CategoryEditor bind:name bind:order {action} />
