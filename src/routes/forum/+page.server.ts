8a861038da3984f55cb92a6940a3def4aff2c200
Vladimir Zarypov <krre31@gmail.com>
2026-02-11 08:33:55 +0200

    Remove unused page server parameter

--- src/routes/forum/+page.server.ts
+++ src/routes/forum/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import * as api from "$lib/api";
 
-export const load: PageServerLoad = async ({ url }) => {
+export const load: PageServerLoad = async () => {
     const getAllResponse = await api.Forum.GetAll.exec();
 
     return {
