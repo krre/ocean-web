d0658841cc46826acdc576e91456b12a0bea0e18
Vladimir Zarypov <krre31@gmail.com>
2026-02-12 05:32:53 +0200

    Add handler for `unhandledRejection` exception

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,13 @@ import type { Handle } from '@sveltejs/kit';
 import { loadSession } from '$lib/session-store';
 
 export const handle: Handle = async ({ event, resolve }) => {
+    process.on('unhandledRejection', (reason, promise) => {
+        console.error('--- DETAILED ERROR ---');
+        console.error('Reason:', reason);
+        console.error('Promise:', promise);
+        console.error('-----------------------');
+    });
+
     const sessionId = event.cookies.get('session_id');
     event.locals.session = await loadSession(sessionId);
     return resolve(event);
