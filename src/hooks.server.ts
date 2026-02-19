bf147fcf7deaab26d9c72ff65e8e1d83a5a6bdfb
Vladimir Zarypov <krre31@gmail.com>
2026-02-19 09:46:35 +0200

    Fix registering `unhandledRejection` exception listerner

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,14 @@
 import type { Handle } from '@sveltejs/kit';
 import { loadSession } from '$lib/session-store';
 
-export const handle: Handle = async ({ event, resolve }) => {
-    process.on('unhandledRejection', (reason, promise) => {
-        console.error('--- DETAILED ERROR ---');
-        console.error('Reason:', reason);
-        console.error('Promise:', promise);
-        console.error('-----------------------');
-    });
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('--- DETAILED ERROR ---');
+    console.error('Reason:', reason);
+    console.error('Promise:', promise);
+    console.error('-----------------------');
+});
 
+export const handle: Handle = async ({ event, resolve }) => {
     const sessionId = event.cookies.get('session_id');
     event.locals.session = await loadSession(sessionId);
     return resolve(event);
