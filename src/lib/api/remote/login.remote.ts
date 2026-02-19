499858b78a5284de7e447526d1c51ee69cbb250a
Vladimir Zarypov <krre31@gmail.com>
2026-02-19 17:26:27 +0200

    Set cookies in `login` remote function

--- src/lib/api/remote/login.remote.ts
+++ src/lib/api/remote/login.remote.ts
@@ -1,8 +1,10 @@
 import * as api from "$lib/api";
 import * as consts from "$lib/consts";
-import { command } from '$app/server';
-import type { UserSession } from '$lib/types';
 import * as v from 'valibot';
+import { getRequestEvent, command } from '$app/server';
+import { saveSession } from '$lib/session-store';
+import type { UserSession } from '$lib/types';
+import uidSafe from 'uid-safe'
 
 export const login = command(v.object({ id: v.number(), token: v.string() }), async ({ id, token }) => {
     const result = await api.User.Auth.exec({ token }, token);
@@ -15,6 +17,20 @@ export const login = command(v.object({ id: v.number(), token: v.string() }), as
         isAnonym: false,
         isAdmin: result.code == consts.Account.Admin,
     };
+
+    const sessionId = uidSafe.sync(24)
+    await saveSession(sessionId, session)
+
+    const { cookies } = getRequestEvent();
+
+    cookies.set('session_id', sessionId, {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: true,
+        maxAge: 60 * 60 * 24 * 365
+    });
+
 
     return session;
 });
