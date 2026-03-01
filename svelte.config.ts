c18edde39cfdc68478508ea5391eb8f7811828f3
Vladimir Zarypov <krre31@gmail.com>
2026-03-01 07:37:41 +0200

    Setup Node deploy

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess({ script: true }),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			out: 'build',
+			precompress: true,
+			envPrefix: ''
+		}),
 		experimental: {
 			remoteFunctions: true
 		}
