a46baf7add0faa3522a78658ce3aef41c3fdf75d
Vladimir Zarypov <krre31@gmail.com>
2026-03-03 07:44:24 +0200

    Allow listen only localhost

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	server: {
 		port: 3000,
+		host: '127.0.0.1',
 	},
 	preview: {
 		port: 3000,
+		host: '127.0.0.1',
 	},
 	plugins: [sveltekit(), devtoolsJson()],
 	test: {
