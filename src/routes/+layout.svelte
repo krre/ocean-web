c844004d80681d5fa65b2b264a0c252787474037
Vladimir Zarypov <krre31@gmail.com>
2026-05-27 06:23:38 +0300

    Move global styles to `layout.css`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Navbar from '$lib/components/menu/Navbar.svelte';
 	import Sidebar from '$lib/components/menu/Sidebar.svelte';
 	import type { LayoutProps } from './$types';
+	import './layout.css';
 
 	let { children }: LayoutProps = $props();
 	let open = $state(false);
@@ -15,63 +16,3 @@
 	{@render children?.()}
 	<Modal />
 </main>
-
-<style>
-	:global(:root) {
-		--border-1px: 1px solid rgba(0, 0, 0, 0.2);
-		--header-background: rgba(0, 0, 0, 0.07);
-		--page-background: rgb(239, 239, 240);
-		--page-padding: 1em;
-		--page-margin: 1em;
-	}
-
-	:global(body) {
-		margin: 0;
-		font-family:
-			Roboto,
-			-apple-system,
-			BlinkMacSystemFont,
-			Segoe UI,
-			Oxygen,
-			Ubuntu,
-			Cantarell,
-			Fira Sans,
-			Droid Sans,
-			Helvetica Neue,
-			sans-serif;
-		font-size: 90%;
-		background-color: var(--page-background);
-	}
-
-	:global(a) {
-		color: teal;
-		text-decoration: none;
-	}
-
-	:global(a:hover) {
-		color: olive;
-		text-decoration: underline;
-	}
-
-	:global(img) {
-		max-width: min(100%, 1000px);
-		max-height: min(100vh, 700px);
-	}
-
-	:global(.video) {
-		max-width: 100%;
-		max-height: auto;
-		width: 420px;
-		height: 315px;
-	}
-
-	:global(.quote) {
-		background-color: rgb(244, 251, 255);
-		border: var(--border-1px);
-		padding: 0.7em;
-	}
-
-	:global(.row-link) {
-		color: rgb(51, 51, 51);
-	}
-</style>
