04159c258609354d567350cf5b98e502fb13cd39
Vladimir Zarypov <krre31@gmail.com>
2026-06-17 08:02:29 +0300

    Add Google Analytics

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
 import type { Handle } from '@sveltejs/kit';
 import { loadSession } from '$lib/session-store';
+import { PUBLIC_GA_ID } from '$env/static/public';
 
 process.on('unhandledRejection', (reason, promise) => {
 	console.error('--- DETAILED ERROR ---');
@@ -11,5 +12,23 @@ process.on('unhandledRejection', (reason, promise) => {
 export const handle: Handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get('session_id');
 	event.locals.session = await loadSession(sessionId);
-	return resolve(event);
+
+	return resolve(event, {
+		transformPageChunk: ({ html }) => {
+			if (!PUBLIC_GA_ID) {
+				return html.replace('%sveltekit.analytics%', '');
+			}
+
+			const analyticsScript = `
+					<script async src="https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GA_ID}"></script>
+					<script>
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', '${PUBLIC_GA_ID}');
+					</script>`;
+
+			return html.replace('%sveltekit.analytics%', analyticsScript);
+		}
+	});
 };
