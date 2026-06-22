b867261f92e029c214249fbfc3cc7e482e127508
Vladimir Zarypov <krre31@gmail.com>
2026-06-22 07:34:05 +0300

    Move Google Analytics scripts to `src/app.html`

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 import { loadSession } from '$lib/session-store';
-import { GOOGLE_ANALYTICS_ID } from '$app/env/public';
 
 process.on('unhandledRejection', (reason, promise) => {
 	console.error('--- DETAILED ERROR ---');
@@ -13,22 +12,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get('session_id');
 	event.locals.session = await loadSession(sessionId);
 
-	return resolve(event, {
-		transformPageChunk: ({ html }) => {
-			if (!GOOGLE_ANALYTICS_ID) {
-				return html.replace('%sveltekit.analytics%', '');
-			}
-
-			const analyticsScript = `
-					<script async src="https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}"></script>
-					<script>
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', '${GOOGLE_ANALYTICS_ID}');
-					</script>`;
-
-			return html.replace('%sveltekit.analytics%', analyticsScript);
-		}
-	});
+	return resolve(event);
 };
